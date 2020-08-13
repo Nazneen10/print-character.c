dd62f293436b9d65cfcf8977a842e0c97aaddb62
Nazneen10 <69063553+Nazneen10@users.noreply.github.com>
2020-08-13 22:04:35 +0530

    Create Print character.c

---
+++ Print character.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+	char ch;
+	printf("enter the alphabet");
+	scanf("%c",&ch);
+	if(ch>='a'&&ch<='b')
+	{
+		printf("lowercase");
+	}
+	else
+	{
+		printf("uppercase");
+	}
+Return 0;
+	}
